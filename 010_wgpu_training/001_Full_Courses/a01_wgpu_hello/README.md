4d56e5bb9d54b430644188e34160e8bd57d45f51
GYoung <ytok1108@aol.com>
2026-09-20 13:07:22 +0900

    wgpu let's go 3

--- 010_wgpu_training/001_Full_Courses/a01_wgpu_hello/README.md
+++ 010_wgpu_training/001_Full_Courses/a01_wgpu_hello/README.md
@@ -8,6 +8,11 @@ Backend:        metal
 Type:           IntegratedGpu
 Driver:
 Max texture dimension 2D:       16384
+Max texture size :              16384
+Max buffer size :               4294967295
+Max bind groups :               8
+Max vertex attribs :            31
+Max compute group :             1024
 
 ```
 
--- 010_wgpu_training/001_Full_Courses/a01_wgpu_hello/src/main.rs
+++ 010_wgpu_training/001_Full_Courses/a01_wgpu_hello/src/main.rs
@@ -22,16 +22,24 @@ fn main() {
 
     for adapter in adapters {
         let info = adapter.get_info();
-        let limits = adapter.limits();
 
         println!("Name:   \t{}", info.name);
         println!("Backend:\t{}", info.backend);
         println!("Type:   \t{:?}", info.device_type);
         println!("Driver: \t{} {}", info.driver, info.driver_info);
 
+        let limits = adapter.limits();
         println!(
             "Max texture dimension 2D:\t{}",
             limits.max_texture_dimension_2d
+        );
+        println!("Max texture size : \t\t{}", limits.max_texture_dimension_2d);
+        println!("Max buffer size : \t\t{}", limits.max_buffer_size);
+        println!("Max bind groups : \t\t{}", limits.max_bind_groups);
+        println!("Max vertex attribs : \t\t{}", limits.max_vertex_attributes);
+        println!(
+            "Max compute group : \t\t{}",
+            limits.max_compute_workgroup_size_x
         );
 
         println!();
